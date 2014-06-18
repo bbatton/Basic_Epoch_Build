78e7b7f553bcbef57188106966460da105644e43
Brandon <bbatton@uncc.edu>
2014-06-17 21:59:00 -0400

    Fixed Window Spam
    Fixed shell spamming new console windows on restart

--- restart.bat
+++ restart.bat
@@ -7,13 +7,13 @@ timeout 5
 :: start the server..
 set dayzpath="C:\Users\grave\Desktop\Dayz Server"
 cd /d %dayzpath%
-start "Dayz Epoch Server" "DayZ_Epoch_instance_11_Chernarus.bat"
+start /b "Dayz Epoch Server" "DayZ_Epoch_instance_11_Chernarus.bat"
  
 timeout 15
 :: start bec
 set becpath="C:\Users\grave\Desktop"
 cd /d %becpath%
-start "BattlEye Extended Control" "BEC control Epoch.bat"
+start /b "BattlEye Extended Control" "BEC control Epoch.bat"
  
 cls
 @exit
